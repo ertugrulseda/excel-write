886ebc2770dee7ab0a6ce2e211b2543b73ad72c1
seda ertugrul <ertugrulseda@gmail.com>
2021-03-15 23:42:58 +0300

    eklendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import React, { useState,useRef } from 'react';
+import ExcelJS from 'exceljs';
 
 function App() {
+  const [ rowData, setRowData ] = useState([]);
+
+  const readCharacters = async () =>{
+   const response = await fetch('https://rickandmortyapi.com/api/character');
+   const data = await response.json();
+   //data.results[0]
+		setRowData([]);
+	};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<div className="Content">
+				<div className="file-input">
+					<button className="btn-clear" onClick={readCharacters}>Karakterleri Doldur</button>
+				</div>
+				<div className="ag-theme-alpine" style={{ height: 400, width: 800 }}>
+					<AgGridReact  rowData={rowData}>
+						<AgGridColumn field="Marka" />
+						<AgGridColumn field="Model" />
+						<AgGridColumn field="Fiyat" />
+						<AgGridColumn field="Plaka" />
+					</AgGridReact>
+				</div>
+			</div>
+		</div>
   );
 }
 
