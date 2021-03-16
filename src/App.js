5515a031c4a54df9bb8e565b754b76001eb7914b
seda ertugrul <ertugrulseda@gmail.com>
2021-03-16 23:24:00 +0300

    sitil düzenlendi,grid dolduruldu

--- src/App.js
+++ src/App.js
@@ -3,36 +3,58 @@ import './App.css';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import React, { useState,useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import ExcelJS from 'exceljs';
 
 function App() {
-  const [ rowData, setRowData ] = useState([]);
+	const [ rowData, setRowData ] = useState([]);
 
-  const readCharacters = async () =>{
-   const response = await fetch('https://rickandmortyapi.com/api/character');
-   const data = await response.json();
-   //data.results[0]
-		setRowData([]);
+	const readCharacters = async () => {
+		const response = await fetch('https://rickandmortyapi.com/api/character');
+		const data = await response.json();
+		let arr = [];
+		data.results.forEach((element) => {
+			const newElement = {
+				ad: element.name,
+				cinsiyet: element.gender,
+				memleket: element.location.name,
+				durum: element.status
+			};
+			arr = [ ...arr, newElement ];
+		});
+		setRowData(arr);
 	};
 
-  return (
-    <div className="App">
-			<div className="Content">
-				<div className="file-input">
-					<button className="btn-clear" onClick={readCharacters}>Karakterleri Doldur</button>
-				</div>
-				<div className="ag-theme-alpine" style={{ height: 400, width: 800 }}>
-					<AgGridReact  rowData={rowData}>
-						<AgGridColumn field="Marka" />
-						<AgGridColumn field="Model" />
-						<AgGridColumn field="Fiyat" />
-						<AgGridColumn field="Plaka" />
-					</AgGridReact>
-				</div>
+	const createExcel = () => {
+		console.log('createExcel');
+	};
+
+	const clear = () => {
+		setRowData([]);
+	};
+	return (
+		<div className="App">
+			<div className="head">
+				<button className="btn-read btn" onClick={readCharacters}>
+					Karakterleri Doldur
+				</button>
+				<button className="btn-excel btn" onClick={createExcel}>
+					Excele Çıkar
+				</button>
+				<button className="btn-clear btn" onClick={clear}>
+					Temizle
+				</button>
+			</div>
+			<div className="ag-theme-alpine grid" style={{ height: 500, width: 1000 }}>
+				<AgGridReact rowData={rowData}>
+					<AgGridColumn field="ad" resizable={true} width={250} />
+					<AgGridColumn field="cinsiyet" resizable={true} />
+					<AgGridColumn field="memleket" resizable={true} width={400} />
+					<AgGridColumn field="durum" resizable={true} />
+				</AgGridReact>
 			</div>
 		</div>
-  );
+	);
 }
 
 export default App;
